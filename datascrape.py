3970809e499c1b30bccda53f65c7c4218bb852c7
poopymcnugget22 <poopymcnugget22@gmail.com>
2026-05-05 20:54:27 +0000

    Reload codespace because of error

--- datascrape.py
+++ datascrape.py
@@ -1,9 +1,18 @@
 from bs4 import BeautifulSoup
+from keras.layers import Dense
+import tensorflow as tf
 import requests
 
 def drink():
+    
     print("Enter a URL.")
     url = input()
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     print("Here is what i fetched for this website: \n" + soup + "Starting AI...")
+
+def ai():
+    tensor = ([[5, 1, 2, 6], [4, 9, 3, 5]])
+    oli = tf.keras.Sequential([tf.keras.layers.Dense(units=64, activation="relu", input_shape=tensor.shape)
+                               tf.keras.layers.Dense(units=64, activation="relu")
+                               tf.keras,layers.Dense(units=1)])
